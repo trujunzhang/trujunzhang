355e9f6704ceedfbfec27fcdc955dd5b58fca883
djzhang <trujunzhang@gmail.com>
2023-09-14 22:15:26 +0800

    xxx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,18 +12,18 @@ type Props = {
 const Hero = ({ pageInfo }: Props) => {
   return (
     <div className="px-8 lg:px-2">
-      <div className="flex flex-col items-center justify-center text-center gap-24 xl:gap-12 md:gap-8">
+      <div className="flex flex-col items-center justify-center text-center gap-24 xl:gap-16 md:gap-12">
         {/* Top Container */}
         <div className="flex flex-col gap-4 justify-center items-center">
           <span className="font-bold text-6xl lg:text-4xl md:text-3xl">
             {pageInfo?.title}
           </span>
-          <p className="font-normal text-3xl lg:text-[22px] sm:max-w-[340px] text-gray-800">
+          <p className="font-normal text-3xl lg:text-2xl md:text-xl sm:max-w-[340px] text-gray-800">
             {pageInfo?.subTitle}
           </p>
         </div>
         {/* Middle Container */}
-        <div className="relative md:px-6 rounded-full border-4 md:border-2 w-56 h-56 md:w-32 md:h-32 sm:w-24 sm:h-24 border-primary-300">
+        <div className="relative md:px-6 rounded-full border-4 md:border-2 w-56 h-56 lg:w-42 lg:h-42 md:w-36 md:h-36 border-primary-300">
           <Image
             className="rounded-full p-1 object-cover"
             src={urlFor(pageInfo.profilePicture).url()}
