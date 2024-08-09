9198cf68df47d27ca061403dcfac2ebec05ab5db
djzhang <trujunzhang@gmail.com>
2024-08-09 11:06:58 +0800

    feat: set 'sanity' client

--- sanity/sanity.client.ts
+++ sanity/sanity.client.ts
@@ -1,8 +1,8 @@
 import {createClient, SanityClient} from 'next-sanity';
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "fxm1fn7l";
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION ||= '2021-10-21';
 
 export const client: SanityClient = createClient({
     projectId,
